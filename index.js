3ec2f810f0bea6672525224fb6ceecd1b7e3b092
karaxuna <kaxabernet@gmail.com>
2015-09-05 23:56:38 +0400

    in progress

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 var express = require('express'),
     app = express(),
-    ghost = require('ghost');
+    ghost = require('ghost'),
+    path = require('path')
 
-ghost().then(function (ghostServer) {
+ghost({
+    config: path.resolve(__dirname, 'config.js')
+}).then(function (ghostServer) {
     app.use(ghostServer.config.paths.subdir, ghostServer.rootApp);
     ghostServer.start(app);
 }).catch(function (err) {
